332cec6dcda49299d901522018148b020df93799
lesakon <ondrej.lesak@storaenso.com>
2020-07-30 20:48:56 +0200

    10 - unsplash app finished

--- src/7/components/image-list.js
+++ src/7/components/image-list.js
@@ -1,11 +1,12 @@
+import './image-list.css'
 import React from "react";
+import ImageCard from "./image-card";
 
 const ImageList = props => {
-  const images = props.images.map(({description, id, urls}) => {
-    return <img key={id} src={urls.regular}
-                alt={description}/>
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image}/>
   })
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
