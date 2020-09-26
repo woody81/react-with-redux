2de43258dc58dace7a84cce1c79eca0348fe43e1
lesakon <ondrej.lesak@storaenso.com>
2020-09-26 20:58:45 +0200

    11,20 - youtube, stream app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './hooks/App'
+import App from './7/components/App'
 
 ReactDOM.render(<App/>, document.getElementById("root"));
